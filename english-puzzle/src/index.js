7ff958fd7f8de4b06f369f3707c045702518fca5
ROnic404 <ronic@inbox.ru>
2020-07-01 22:03:26 +0400

    feat: add some changes 2

--- english-puzzle/src/index.js
+++ english-puzzle/src/index.js
@@ -1,12 +1,11 @@
 import './sass/main.scss';
 
 import hintsSwitcher from './js/hintsSwitcher.js';
-import './js/render/renderResults.js';
-
+import renderResults from './js/render/renderResults.js';
 import pageSwitcher from './js/pageSwitcher.js';
-
 
 window.addEventListener('load', () => {
   pageSwitcher();
   hintsSwitcher();
+  renderResults();
 });
